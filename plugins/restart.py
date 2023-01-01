399d163ca97057dac735e9e1045200261f5f79dd
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-01 16:16:06 +0300

    Create restart.py

---
+++ plugins/restart.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from sys import executable
+import os
+@Client.on_message(filters.private & filters.command("restart"))
+async def restart(app, message):
+    cmd = message.text.split(' ', 1)
+    dynoRestart = False
+    dynoKill = False
+    if len(cmd) == 2:
+        dynoRestart = (cmd[1].lower()).startswith('d')
+        dynoKill = (cmd[1].lower()).startswith('k')
+    try:
+        await message.reply_text("Normal Restart oluyor.")
+        os.execl(executable, executable, "bot.py")
+    except Exception as f
+        await message.reply_text("başaramadım")
