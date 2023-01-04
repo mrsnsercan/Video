e8e2dd20651541c885e0777c9cdf635501240ec8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-04 22:36:42 +0300

    Create status.py

---
+++ plugins/status.py
@@ -0,0 +1,25 @@
+import shutil
+import psutil
+import math
+
+from config import humanbytes, quee
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command("status"))
+async def status(app, message):
+    msg = await m.reply_text(text="`Bekle 😊😇🙃`")
+    toplam, kullanilan, bos = shutil.disk_usage(".")
+    toplam = humanbytes(toplam)
+    kullanilan = humanbytes(kullanilan)
+    bos = humanbytes(bos)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    text = f"**Toplam Disk Alanı:** `{toplam}` \n"
+    text += f"**Kullanılan Alan:** `{kullanilan}({disk_usage}%)` \n"
+    text += f"**Boş Alan:** `{bos}` \n"
+    text += f"**CPU Kullanımı:** `{cpu_usage}%` \n"
+    text += f"**RAM Kullanımı:** `{ram_usage}%`\n\n"
+    text += f"**Yapacak {len(quee)} işim var.**" 
