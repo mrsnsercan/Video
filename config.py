698480d7437a828a80b1bcfbccd4e0e978dd8c2c
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-05 11:20:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,12 @@ DOWNLOAD_DIR = environ.get("DOWNLOAD_DIR", "downloads")
 ENCODE_DIR = environ.get("ENCODE_DIR", "encodes")
 SUDO_USERS = list(set(int(x) for x in environ.get("SUDO_USERS").split()))
 PRE_LOG = environ.get("PRE_LOG", "")
-STRING_SESSION = environ.get("STRING_SESSION", "")
-
-userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
-userbot.start()
-print("Userbot Başladı")
+try:
+    STRING_SESSION = environ.get('USER_SESSION_STRING', '')
+    if len(STRING_SESSION) != 0:
+        userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+        userbot.start()
+    if not userbot:
+        print("session hatalı tekrar al") 
+    else:
+        print("Userbot Başlatıldı 4 gb yükleme aktif")
