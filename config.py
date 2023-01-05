175192390347123431d3fedbd47c279a6ed3bd0d
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-05 11:28:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -29,10 +29,6 @@ SUDO_USERS = list(set(int(x) for x in environ.get("SUDO_USERS").split()))
 PRE_LOG = environ.get("PRE_LOG", "")
 try:
     STRING_SESSION = environ.get('USER_SESSION_STRING', '')
-    if len(STRING_SESSION) != 0:
         userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
-    if not userbot:
-        print("session hatalı tekrar al") 
-    else:
         userbot.start()
         print("Userbot Başlatıldı 4 gb yükleme aktif")
